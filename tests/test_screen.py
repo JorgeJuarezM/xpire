dc24090b89953d56e79d3c22e86a0284171ccedd
Jorge Juarez <contacto@jorgejuarez.net>
2025-02-12 21:28:43 -0600

    feat: add unit test for screen

---
+++ tests/test_screen.py
@@ -0,0 +1,14 @@
+import unittest
+
+from xpire.cpus.intel_8080 import Intel8080, Registers
+from xpire.memory import Memory
+from xpire.screen import Screen
+
+
+class TestIntel8080(unittest.TestCase):
+    def test_screen(self):
+        memory = Memory()
+        cpu = Intel8080(memory=memory)
+
+        screen = Screen(width=224, height=256, title="Xpire", scale=3)
+        screen.render(cpu)
--- xpire/screen.py
+++ xpire/screen.py
@@ -1,4 +1,5 @@
 import pygame
+
 from xpire.cpus.cpu import AbstractCPU
 
 
