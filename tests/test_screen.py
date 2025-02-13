553124c5505f25a7718b550c0b612cbc55c0ba4c
Jorge Juarez <contacto@jorgejuarez.net>
2025-02-12 23:50:43 -0600

    chore: improve unit test

--- tests/test_screen.py
+++ tests/test_screen.py
@@ -2,13 +2,14 @@
 Test class for the Intel8080 CPU.
 """
 
+import math
 import unittest
 
 from xpire.cpus.cpu import CPU
 from xpire.memory import Memory
 from xpire.screen import Screen
 
-WHITE_COLOR = (255, 255, 255)
+WHITE_COLOR = (255, 255, 255, 255)
 
 
 class TestIntel8080(unittest.TestCase):
@@ -29,12 +30,18 @@ class TestIntel8080(unittest.TestCase):
         """
 
         for i in range(0x2400, 0x4000):
-            self.memory[i] = 0xFF
+            self.memory[i] = 0xFF if i % 2 == 0 else 0x00
 
         screen = Screen(width=224, height=256, title="Xpire", scale=3)
         screen.color_table = [WHITE_COLOR]
         screen.render(self.cpu)
 
         assert screen._screen.get_size() == (224, 256)
-        for i in range(0, 256):
-            assert screen._screen.get_at((10, i)) == WHITE_COLOR
+
+        counter = 0
+        for x in range(0, screen._screen.get_width()):
+            for y in range(0, screen._screen.get_height()):
+                byte_index = math.floor(counter / 8)
+                if byte_index % 2 == 1:
+                    assert screen._screen.get_at((x, y)) == WHITE_COLOR
+                counter += 1
