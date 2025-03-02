7c11a9d25d58b2710bc18b016f7b1cbce85d07a2
Jorge Juarez <contacto@jorgejuarez.net>
2025-03-02 15:33:37 -0600

    chore: reset changes on rst

--- xpire/cpus/intel_8080.py
+++ xpire/cpus/intel_8080.py
@@ -910,9 +910,6 @@ class Intel8080(CPU):
 
     @manager.add_instruction(OPCodes.RST_1)
     def rst_1(self) -> None:
-        if not self.interrupts_enabled:
-            return
-
         self.interrupts_enabled = False
         h, l = split_word(self.PC)
         self.push(h, l)
@@ -921,9 +918,6 @@ class Intel8080(CPU):
 
     @manager.add_instruction(OPCodes.RST_2)
     def rst_2(self) -> None:
-        if not self.interrupts_enabled:
-            return
-
         self.interrupts_enabled = False
         h, l = split_word(self.PC)
         self.push(h, l)
@@ -998,9 +992,6 @@ class Intel8080(CPU):
 
     @manager.add_instruction(OPCodes.RST_7)
     def rst_7(self) -> None:
-        if not self.interrupts_enabled:
-            return
-
         self.interrupts_enabled = False
         h, l = split_word(self.PC)
         self.push(h, l)
